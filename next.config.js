8b7fd97e00a133c5c9ca298d9889ef6c5ec8b95f
juanrra17 <juan.lopez@parqueexplora.org>
2024-01-03 17:46:46 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'main/styles')],
+    prependData: `@import "_main.scss";`,
+  },
+  images: {
+    domains: [
+      'imgix.cosmicjs.com',
+      'lh3.googleusercontent.com',
+      'cdn.sanity.io',
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {};
+    config.optimization.minimize = true;
+    return config;
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com', 'cdn.sanity.io',],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
